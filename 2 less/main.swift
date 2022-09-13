ca13a1ba470f2a1b475cb5825f39c6508e2c26f5
Ajo Almasbekov <ajo.altyn@gmail.com>
2022-09-13 21:16:32 +0600

    Dom z 2

--- 2 less/main.swift
+++ 2 less/main.swift
@@ -7,9 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+func arif (num: Int) {
+    print("квадрат \(num) равен \(num * num)")
+}
 
-print("Hey")
+arif(num: 5)
 
+func cub (num: Int) {
+    print("куб \(num) равен \(num * num * num)")
+}
+
+cub(num: 4)
+
+func perCub (num:Int) {
+    print("Р=12*\(num) = \(12*num)")
+}
+perCub(num: 10)
+
+func areaRect (side1:Int, side2:Int) {
+    print("S = Длина * Ширину = \(side1 * side2)")
+    
+}
+areaRect(side1: 5, side2: 6)
 
 
